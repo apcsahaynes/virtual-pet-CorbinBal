9742e19763f7d88f9c1874069f46be37ec30cabb
CorbinBal <corbin.balukonis@jpschools.org>
2026-03-11 11:21:37 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,27 @@
 public class Game {
+     private String name;
+    private int happinessIncrease;
+    private int weightDecrease;
+    private double chance;
+    public Game(String name, int happinessIncrease, int weightDecrease, double chance){
+        this.name = name;
+        this.happinessIncrease = happinessIncrease;
+        this.weightDecrease = weightDecrease;
+        this.chance = chance;    }
     
+    public String getName(){
+        return name;
+    }
+    
+    public int getHappinessIncrease(){
+        return happinessIncrease;
+    }
+    
+    public int getWeightDecrease(){
+        return weightDecrease;
+    }
+    
+    public boolean isWinner(){
+        return Math.random() < chance;
+    }
 }
